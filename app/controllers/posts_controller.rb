931ebbdb2f74f73a9d785e27b2dde5236d229356
musabgelisgen <mgelisgen97@gmail.com>
2018-08-22 18:30:35 +0300

    Blog edit enabled

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,10 +26,17 @@ class PostsController < ApplicationController
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if (@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
 
   end
 
